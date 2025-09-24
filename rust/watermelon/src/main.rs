aa6f88fa78bb3faa038291cde441b8dbd82f935d
Tarek Wfa <tarekmohamedwfa@gmail.com>
2025-09-25 02:33:39 +0300

    Solve Codeforces problem 4A - Watermelon

--- rust/watermelon/src/main.rs
+++ rust/watermelon/src/main.rs
@@ -1,5 +1,13 @@
 use std::io;
 
 fn main(){
-    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to readline");
+    let number: i32 = input.trim().parse().expect("This is not a number!!");
+    if number != 2 && number%2 == 0{
+        println!("YES");
+    }
+    else{
+        println!("NO");
+    }
 }
